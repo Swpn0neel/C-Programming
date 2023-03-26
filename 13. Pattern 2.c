848868d8138baa9c8dbbbbea715547d68e4dc597
Swapnoneel Saha <121167506+Swpn0neel@users.noreply.github.com>
2023-03-26 14:32:09 +0000

    Added Pattern 2 program

---
+++ 13. Pattern 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+    for(int i=5; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            printf(" ");
+        }
+        for(int j=5; j>=i; j--){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
