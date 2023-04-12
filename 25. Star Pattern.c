852d4244ba10831a634e5c76136244a0d71b4906
Swapnoneel Saha <121167506+Swpn0neel@users.noreply.github.com>
2023-04-12 09:21:50 +0000

    Added a program

--- 25. Star Pattern.c
+++ 25. Star Pattern.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 void main(){
-    for(int i=5; i>=0; i--){
+    for(int i=3; i>=0; i--){
         for(int j=1; j<=i; j++){
             printf(" ");
         }
-        for(int j=5; j>=i; j--){
+        for(int j=3; j>=i; j--){
             printf("* ");
         }
         printf("\n");
     }
-    for(int i=1; i<=5; i++){
+    for(int i=1; i<=3; i++){
         for(int j=1; j<=i; j++){
             printf(" ");
         }
-        for(int j=5; j>=i; j--){
+        for(int j=3; j>=i; j--){
             printf("* ");
         }
         printf("\n");
